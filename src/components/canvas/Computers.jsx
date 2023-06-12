2d7cefd143c2d6e47cdbb10509a53d71c6a4d721
maimohammed928 <maimohammed928@gmail.com>
2023-06-12 18:23:54 +0300

    width

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -33,7 +33,7 @@ const ComputersCanvas = () => {
 	
 	const [isMobile, setIsMobile] = useState(false);
 	useEffect(() => {
-		const mediaQuery = window.matchMedia("(max-width:300px)");
+		const mediaQuery = window.matchMedia("(max-width:500px)");
 		setIsMobile(mediaQuery.matches);
 
 		const handleMediaQueryChange = (e) => {

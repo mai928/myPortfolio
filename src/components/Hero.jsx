348b009e7907cc788ee75669d2177cfa714a37d6
maimohammed928 <maimohammed928@gmail.com>
2023-11-21 23:50:27 +0200

    change details in cv

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,16 +24,13 @@ const Hero = () => {
 					</p>
 				</div>
 			</div>
-		
-	
-										<ComputersCanvas />
 
-	
+			<ComputersCanvas />
 
-		
-
-			<div className="absolute xs:-bottom-0
-			 bottom-32 w-full flex justify-center items-center ">
+			<div
+				className="absolute xs:-bottom-0
+			 bottom-32 w-full flex justify-center items-center "
+			>
 				<a href="#about">
 					<div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
 						<motion.div
